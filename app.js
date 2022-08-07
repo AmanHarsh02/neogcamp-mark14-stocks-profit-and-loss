0df435c260abe2f895ee5f9993cc90ba14d4b865
Aman Harsh <amanharsh0399@gmail.com>
2022-08-08 01:00:37 +0530

    update: completed with functionality and styling

---
+++ app.js
@@ -0,0 +1,81 @@
+const initialPriceRef = document.querySelector("#initial-price-input");
+const stocksQuantityRef = document.querySelector("#stocks-quantity-input");
+const currentPriceRef = document.querySelector("#current-price-input");
+const submitBtnRef = document.querySelector("#submit-btn");
+const outputRef = document.querySelector("#output-div");
+
+function calculateProfitOrLoss(initial, quantity, current) {
+    if (current > initial) {
+        var profitValue = (current - initial) * quantity;
+        var profitPercentage = (profitValue / initial) * 100;
+
+        const message = `Whoa! You invested in great stocks. 💯\nThe profit is ${profitValue.toFixed(2)} and the percent is ${profitPercentage.toFixed(2)}% 📈`;
+
+        document.getElementById("output-background-div").style.backgroundImage = "url('/background-images/stonks_profit.png')";
+
+        document.getElementById("output-background-div").style.padding = "4rem";
+
+        document.getElementById("output-div").style.boxShadow = "0px 0px 20px black";
+
+        document.getElementById("output-div").style.backgroundColor = 'rgba(255, 255, 255, 0.6)';
+
+        document.getElementById("output-div").style.color = "black";
+
+        showOutputMessage(message);
+
+    } else if (initial > current) {
+        var lossValue = (initial - current) * quantity;
+        var lossPercentage = (lossValue / initial) * 100;
+
+        const message = `Oops! You should look for some good stocks 😔.\nThe loss is ${lossValue.toFixed(2)} and the percent is ${lossPercentage.toFixed(2)}% 📉`;
+
+        document.getElementById("output-background-div").style.backgroundImage = "url('/background-images/stonks_loss.jpg')";
+
+        document.getElementById("output-background-div").style.padding = "4rem";
+
+        document.getElementById("output-div").style.boxShadow = "0px 0px 20px black";
+
+        document.getElementById("output-div").style.backgroundColor = 'rgba(255, 255, 255, 0.6)';
+
+        document.getElementById("output-div").style.color = "black";
+
+        showOutputMessage(message);
+
+    } else {
+        const message = "Please don't invest in dead stocks. 🪦"
+        showOutputMessage(message);
+    }
+}
+
+function showOutputMessage(message) {
+    outputRef.innerText = message;
+}
+
+function clickHandler() {
+    const initial = Number(initialPriceRef.value);
+    const quantity = Number(stocksQuantityRef.value);
+    const current = Number(currentPriceRef.value);
+
+    showOutputMessage("");
+
+    document.getElementById("output-background-div").style.backgroundImage = "";
+
+    document.getElementById("output-background-div").style.color = "";
+
+    document.getElementById("output-background-div").style.padding = "";
+
+    document.getElementById("output-div").style.boxShadow = "";
+
+    document.getElementById("output-div").style.backgroundColor = "";
+
+    document.getElementById("output-div").style.color = "";
+
+    if (initial && quantity && current) {
+        calculateProfitOrLoss(initial, quantity, current);
+    } else {
+        const message = "Please enter all the values."
+        showOutputMessage(message);
+    }
+}
+
+submitBtnRef.addEventListener("click", clickHandler);
